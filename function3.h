a52e23cdfbef124ef46e3662cfb87ed9e3deb6f2
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2023-10-28 09:07:38 +0700

    Add files via upload

---
+++ function3.h
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+void function3() {
+    // Create a vector to store group submissions
+    vector<GroupSubmission> groupSubmissions;
+
+    // Create a vector of Project structures
+    vector<Project> projects;
+    ifstream file("Prj.txt");   
+    if (file.is_open()) {
+        while (!file.eof()) {
+            Project project;
+            string line;
+
+            // Read project number
+            getline(file, line);
+            if (line.empty()) {
+                break; // Stop if no more projects
+            }
+            project.number = stoi(line.substr(15)); // Extract number from "Project Number: X"
+
+            // Read description
+            getline(file, project.description);
+
+            // Read deadline
+            getline(file, project.deadline);
+            projects.push_back(project);
+        }
+        file.close();
+
+    
+
+    int groupNumber, projectNumber;
+    string submissionDate;
+
+    cout << "Enter the group number: ";
+    cin >> groupNumber;
+
+    cout << "Enter the project number: ";
+    cin >> projectNumber;
+
+    cout << "Enter the submission date (DD/MM/YYYY): ";
+    cin >> submissionDate;
+
+    bool found = false;
+    for (const Project& project : projects) {
+        if (project.number == projectNumber) {
+            GroupSubmission submission;
+            submission.groupNumber = groupNumber;
+            submission.projectnumber = projectNumber;
+            submission.submissionDate = submissionDate;
+            groupSubmissions.push_back(submission);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Project with number " << projectNumber << " not found.\n";
+    } else {
+        cout << "Submission recorded for Group " << groupNumber << " and Project " << projectNumber << ".\n";
+    }
+}
+    ofstream infile("GroupSubmissions.txt", ios::app);
+
+    if (infile.is_open()) {
+        for (const GroupSubmission& submission : groupSubmissions) {
+            infile << "Group Number: " << submission.groupNumber << "\n";
+            infile << "Project Number: " << submission.projectnumber << "\n";
+            infile <<  submission.submissionDate << "\n\n";
+        }
+
+        file.close();
+        cout << "Group submissions have been saved to GroupSubmissions.txt.\n";
+    } else {
+        cerr << "Unable to open GroupSubmissions.txt for writing.\n";
+    }
+}
